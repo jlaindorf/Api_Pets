2cace1144dd419ca6bfb336c57050532ea415b29
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-11-14 21:19:04 -0300

    cria metodo de response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,17 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function response($message, $data, $status = true, $statusCode = 200){
+        $response =[
+
+            'sucess' => $status,
+            'message' => $message,
+            'data' => $data,
+        ];
+
+        return response($response, $statusCode);
+
+
+    }
 }
